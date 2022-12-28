fab24dec4be8a94cdf6812db4a94fb6a4b51551f
MiguelSEO1 <miguelsanlucarlebron@gmail.com>
2022-12-28 16:27:47 +0000

    proyecto simple contador terminado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,54 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, {useState, useEffect} from "react";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+       
+	const [timer, setTimer] = useState(0);
+	const [isRunning, setIsRunning] = useState(true); // nueva variable de estado para indicar si el cronómetro está en funcionamiento o no
+
+	useEffect(() => {
+		
+		let interval;
+		if (isRunning == false) {
+		interval = setInterval(() => {
+		setTimer((X) => X + 1);
+		}, 100);
+		} else {
+		clearInterval(interval);
+		}
+		return () => clearInterval(interval);
+		}, [isRunning]);
+
+
+	const reset = () => {
+			    setTimer (0)
+				setIsRunning(true);
+				};		
+	
+return (
+		<div className=" m-5 p-5 container-flex">
+			<div className="container-sm d-flex flex-row justify-content-center bg-dark p-5">
+				<div className="icon text-white container-flex border border-light alert alert-dark">
+				<i class="fa-solid fa-clock"></i></div>
+				
+				<div className="digit text-white p-5 fs-1 border border-light">{Math.floor(timer/100000)%10}</div>
+				<div className="digit text-white p-5 fs-1 border border-light">{Math.floor(timer/10000)%10}</div>
+				<div className="digit text-white p-5 fs-1 border border-light">{Math.floor(timer/1000)%10}</div>
+				<div className="digit text-white p-5 fs-1 border border-light">{Math.floor(timer/100)%10}</div>
+				<div className="digit text-white p-5 fs-1 border border-light">{Math.floor(timer/10)%10}</div>
+				<div className="digit text-white p-5 fs-1 border border-light">{Math.floor(timer/1)%10}</div>
+		
+			</div>
+			<div class="d-grid gap-2 m-5">
+			
+			<button className={`btn ${isRunning ? "bg-primary" : "bg-danger"} btn-lg`} onClick={() =>setIsRunning(!isRunning)}>{isRunning ? "Start" : "Stop"}</button>
+            <button className="btn btn-success btn-lg" onClick={reset}>Reset</button>
+			
+			</div>
+		
 		</div>
 	);
 };
+
 
 export default Home;
